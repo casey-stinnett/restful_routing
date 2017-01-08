65d28ec4c424167867d1ea4a2aed3bf3e8490486
Casey Stinnett <casey.stinnett@nycda.com>
2017-01-08 14:40:13 -0700

    Got rid of todo in gemspec and added listen code

--- lib/restful_routing.rb
+++ lib/restful_routing.rb
@@ -1,5 +1,10 @@
 require "restful_routing/version"
 
 module RestfulRouting
-  # Your code goes here...
+  listener = Listen.to(Dir.pwd + '/config/routes.rb') do |modified, added, removed|
+    puts "modified absolute path: #{modified}"
+    puts "added absolute path: #{added}"
+    puts "removed absolute path: #{removed}"
+  end
+  listener.start # not blocking
 end
