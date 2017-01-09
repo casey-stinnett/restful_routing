46aacb1bab33d093eb1b28ad1f3d54ce3a40f5a8
Casey Stinnett <casey.stinnett@nycda.com>
2017-01-09 00:11:57 -0700

    made Rails.root into string with Rails.root.to_s

--- lib/restful_routing.rb
+++ lib/restful_routing.rb
@@ -2,7 +2,7 @@ require "restful_routing/version"
 require 'listen'
 
 module RestfulRouting
-  base_path = Rails.root
+  base_path = Rails.root.to_s
   listener = Listen.to(base_path + '/config/', only: /routes\.rb$/) do |modified, added, removed|
   	#run `rake routes` and put the output in a file called
   	#restful_routing.rb
