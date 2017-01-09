b79692f82cb95d625fe71c880cd13eb56bc18128
Casey Stinnett <casey.stinnett@nycda.com>
2017-01-09 12:26:37 -0700

    got the directory to work and restful routing adds

--- lib/restful_routing.rb
+++ lib/restful_routing.rb
@@ -4,7 +4,6 @@ require 'listen'
 module RestfulRouting
   base_path = Dir.pwd
   listening_to = base_path + '/config'
-  puts "HERE IS THE PATH THAT COMES OUT: #{listening_to}"
   listener = Listen.to(listening_to, only: /routes\.rb$/) do |modified, added, removed|
   	#run `rake routes` and put the output in a file called
   	#restful_routing.rb
--- lib/restful_routing/version.rb
+++ lib/restful_routing/version.rb
@@ -1,3 +1,3 @@
 module RestfulRouting
-  VERSION = "0.1.3.4"
+  VERSION = "1.0"
 end
